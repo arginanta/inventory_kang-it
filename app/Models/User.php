0bdf75bc12be17059d167750270e5d717e134073
arginanta <arginanta30@gmail.com>
2022-03-23 18:26:24 +0700

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'level',
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+}
